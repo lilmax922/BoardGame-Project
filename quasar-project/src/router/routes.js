317ded3c2ed0d4d99ce3adda419194dc91b757da
Max <asd88922@gmail.com>
2023-02-03 14:15:31 +0800

    upload

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -1,4 +1,5 @@
 import MainLayout from 'src/layouts/MainLayout.vue'
+import ErrorNotFound from 'src/pages/ErrorNotFound.vue'
 
 const routes = [
   {
@@ -10,14 +11,27 @@ const routes = [
         name: 'Home',
         component: () => import('pages/IndexPage.vue'),
         meta: {
-          title: '差滴滴'
+          title: '差滴滴',
+          login: false,
+          admin: false
         }
       }
     ]
   },
   {
+    path: '/404',
+    name: '404',
+    component: ErrorNotFound,
+    meta: {
+      title: '差滴滴 | 404',
+      login: false,
+      admin: false
+    }
+  },
+  {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
+    name: 'all',
+    redirect: '/404'
   }
 ]
 
