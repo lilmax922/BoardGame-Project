00787acb15678eb02d42497e3be0aeae04e6ea3e
Max <asd88922@gmail.com>
2023-02-08 16:34:05 +0800

    upload

--- quasar-project/quasar.config.js
+++ quasar-project/quasar.config.js
@@ -31,7 +31,8 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'i18n',
-      'axios'
+      'axios',
+      'img-input'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
