3979c5f99acbb5295b847d29e4bebcf2cfafe91d
Max <asd88922@gmail.com>
2023-02-10 02:26:16 +0800

    20230210 upload

--- quasar-project/src/pages/frontPages/ReservationPage.vue
+++ quasar-project/src/pages/frontPages/ReservationPage.vue
@@ -3,10 +3,17 @@
 </script>
 
 <template lang="pug">
-q-page#reservation
-  h1 reservation
+q-page#reservation(padding)
+  .container.q-gutter-md
+    section
+      q-breadcrumbs
+        q-breadcrumbs-el(icon='home' to='/')
+        q-breadcrumbs-el(label='手刀預約' icon='mdi-calendar')
+    section.q-mt-lg
+      .text-h3 預約須知
+    section
+      q-date
 </template>
 
 <style>
-
 </style>
