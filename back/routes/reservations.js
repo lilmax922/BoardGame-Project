cb51ab64c404692a1ec84462df7543a6f13c61a1
Max <asd88922@gmail.com>
2023-02-12 03:49:42 +0800

    20230212 add post route

--- back/routes/reservations.js
+++ back/routes/reservations.js
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import content from '../middleware/content.js'
 import { jwt } from '../middleware/auth.js'
+// import admin from '../middleware/admin.js'
 import { createReservation } from '../controllers/reservations.js'
 
 const router = Router()
 
-router.post('/', content('multipart/form-data'), jwt, createReservation)
+router.post('/', content('application/json'), jwt, createReservation)
+// router.get('/all', jwt, getAllReservations)
+// router.get('/adminAll', jwt, admin, getAdminAllReservations)
+// router.get('/:id', jwt, getReservation)
+// router.patch('/delete/:id', content('application/json'), jwt, deleteReservation)
 
 export default router
