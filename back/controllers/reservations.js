df54e2418f473a89957b61510690434720faf277
Max <asd88922@gmail.com>
2023-02-13 21:58:36 +0800

    更改 create 跟 get

--- back/controllers/reservations.js
+++ back/controllers/reservations.js
@@ -4,9 +4,9 @@ export const createReservation = async (req, res) => {
   try {
     const result = await reservations.create({
       reserver: req.user._id,
-      selectedDate: req.body.selectedDate,
-      selectedTime: req.body.selectedTime,
-      hours: req.body.hours,
+      date: req.body.selectedDate,
+      time: req.body.selectedTime,
+      hour: req.body.selectedHour,
       totalPeople: req.body.totalPeople
     })
     res.status(200).json({ success: true, message: '預約成功', result })
@@ -28,8 +28,8 @@ export const createReservation = async (req, res) => {
 
 export const getReservation = async (req, res) => {
   try {
-    const result = await reservations.find({ selectedDate: req.body.selectedDate }).select('selectedTime hours')
-    // req.body.selectedDate 撈 整筆資料
+    // req.body.selectedDate 會撈到 model 的 reservations 整筆資料，取裡頭的 selectedTime 跟 hours
+    const result = await reservations.find({ date: req.body.selectedDate }).select('time hour')
     res.status(200).json({ success: true, message: '查詢成功', result })
   } catch (error) {
     if (error.name === 'ValidationError') {
@@ -38,7 +38,7 @@ export const getReservation = async (req, res) => {
       res.status(400).json({ success: false, message })
     } else if (error.name === 'MongoServerError' && error.code === 11000) {
       // 代表重複
-      res.status(400).json({ success: false, message: '桌遊名稱重複' })
+      res.status(400).json({ success: false, message: '名稱重複' })
     } else {
       res.status(500).json({ success: false, message: '未知錯誤' })
     }
