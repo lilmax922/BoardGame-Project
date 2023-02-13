e5acc7ef005b4deb6adcc93c6b98424d9e5aec44
Max <asd88922@gmail.com>
2023-02-13 21:56:35 +0800

    改預約的 Model Schema 名稱

--- back/models/reservations.js
+++ back/models/reservations.js
@@ -22,22 +22,17 @@ const schema = new Schema(
     //     message: '手機號碼格式錯誤'
     //   }
     // },
-    selectedDate: {
+    date: {
       type: Date,
       default: Date.now, // 當下時間戳記 ; Date.now() 會變成 server 打開的時間導致每個時間都一樣
+      required: [true, '缺少預約日期']
+    },
+    time: {
+      type: String,
+      enum: ['10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM', '08:00 PM'],
       required: [true, '缺少預約時間']
     },
-    selectedTime: {
-      type: String,
-      enum: ['10:00 AM', '11:00 AM', '12:00 PM', '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM', '08:00 PM']
-      // required: [true, '缺少預約時間']
-    },
-    // endDate: {
-    //   type: Date,
-    //   default: Date.now,
-    //   required: [true, '缺少預計結束時間']
-    // },
-    hours: {
+    hour: {
       type: Number,
       default: 1,
       required: [true, '缺少預約時數']
