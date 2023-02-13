b436774fc9ffad687b75399525abb8129114701e
Max <asd88922@gmail.com>
2023-02-13 11:03:32 +0800

    add selectedTime

--- back/controllers/reservations.js
+++ back/controllers/reservations.js
@@ -5,6 +5,7 @@ export const createReservation = async (req, res) => {
     const result = await reservations.create({
       reserver: req.user._id,
       selectedDate: req.body.selectedDate,
+      selectedTime: req.body.selectedTime,
       period: req.body.period,
       totalPeople: req.body.totalPeople
     })
@@ -24,3 +25,22 @@ export const createReservation = async (req, res) => {
 }
 
 // TODO: getReservation,getAllReservations,deleteReservation,editReservation
+
+export const getReservation = async (req, res) => {
+  try {
+    const result = await reservations.find({ selectedDate: req.body.selectedDate }).select('selectedTime period')
+    // req.body.selectedDate 撈 整筆資料
+    res.status(200).json({ success: true, message: '查詢成功', result })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400).json({ success: false, message })
+    } else if (error.name === 'MongoServerError' && error.code === 11000) {
+      // 代表重複
+      res.status(400).json({ success: false, message: '桌遊名稱重複' })
+    } else {
+      res.status(500).json({ success: false, message: '未知錯誤' })
+    }
+  }
+}
