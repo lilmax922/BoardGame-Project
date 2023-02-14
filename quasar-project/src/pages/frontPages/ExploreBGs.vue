39ab0f2840a485fd5e9dc1283c20f0e22b6a9de1
Max <asd88922@gmail.com>
2023-02-14 16:27:16 +0800

    change layout

--- quasar-project/src/pages/frontPages/ExploreBGs.vue
+++ quasar-project/src/pages/frontPages/ExploreBGs.vue
@@ -23,19 +23,6 @@ const form = reactive({
 
 </script>
 
-<!-- <template lang="pug">
-q-page#exploreBGs
-  .container
-    .flex.items-center
-      .text-h3.q-pr-md(style="display:inline-block") 探索桌遊
-      q-btn(label="我要預約" to="/reservation" color="primary")
-    div.q-mt-lg.flex.column
-      div.q-gutter-md(v-for="boardgame in boardgames" :key="boardgame._id")
-        BoardgameCard.col-8(v-bind="boardgame")
-      div
-        q-input(name="search")
-</template> -->
-
 <template>
   <q-page id="explore-boardgames" padding>
     <div class="q-ma-lg">
@@ -51,18 +38,11 @@ q-page#exploreBGs
         </div>
         <q-btn label="我要預約" to="/reservation" color="primary" />
       </section>
-      <section class="page-body flex">
-        <div id="cards-container">
-          <div class="row ">
-            <div class="col-12 col-md-6 col-lg-4 col-xl-3" v-for="boardgame in boardgames" :key="boardgame._id">
-              <BoardgameCard class="bg-card q-mb-lg" v-bind="boardgame" />
-            </div>
-          </div>
-        </div>
+      <section class="page-body">
         <div id="search-container">
           <div id="search-bar">
             <q-input name="search" placeholder="搜尋" outlined>
-              <template v-slot:append>
+              <template v-slot:prepend>
                 <q-icon name="search" />
               </template>
             </q-input>
@@ -73,32 +53,35 @@ q-page#exploreBGs
                 <q-icon class="q-pl-md" name="mdi-google-downasaur" size="sm" />
                 <span class="text-h6 q-pa-md">桌遊類型</span>
               </div>
-              <div class="flex flex-center q-gutter-sm">
-                <q-btn v-model="filterTypes" v-for="(type, idx) in filterTypes" :key="idx" :label="type"
-                  color="primary" />
+              <div class="flex flex-center q-gutter-md">
+                <!-- <q-btn v-model="filterTypes" v-for="(type, idx) in filterTypes" :key="idx" :label="type" color="primary" size="lg" /> -->
               </div>
             </div>
             <div id="players">
               <div class="flex items-center">
                 <q-icon class="q-pl-md" name="mdi-account-group" size="sm" />
                 <div class="text-h6 q-pa-md">遊玩人數</div>
-                <q-range v-model="form.players" class="q-px-lg" :min="1" :max="15" markers marker-labels
-                  thumb-color="secondary" label-always :step="2"  />
+                <!-- <q-range v-model="form.players" class="q-px-lg" :min="1" :max="15" markers marker-labels
+                  thumb-color="secondary" label-always :step="2" /> -->
               </div>
             </div>
             <div id="game-time">
               <div class="flex items-center">
                 <q-icon class="q-pl-md" name="mdi-timer-sand" size="sm" />
                 <div class="text-h6 q-pa-md">遊戲時間</div>
-                <q-range v-model="form.gameTime" class="q-px-lg" :min="10" :max="60" markers marker-labels
-                  thumb-color="secondary" label-always :step="10" />
+                <!-- <q-range v-model="form.gameTime" class="q-px-lg" :min="10" :max="60" markers marker-labels
+                  thumb-color="secondary" label-always :step="10" /> -->
               </div>
             </div>
           </div>
         </div>
-      </section>
-      <section id="">
-
+        <div id="cards-container">
+          <div class="row q-gutter-sm-md">
+            <div class="col-12 col-md-6 col-lg-3" v-for="boardgame in boardgames" :key="boardgame._id">
+              <BoardgameCard class="bg-card q-mb-lg" v-bind="boardgame" />
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   </q-page>
@@ -106,30 +89,24 @@ q-page#exploreBGs
 
 <style lang="scss">
 #explore-boardgames {
+  width: 100%;
+
   .header {
     padding-bottom: 55px;
   }
 
   .page-body {
     width: 100%;
-
     #cards-container {
-      width: 85%;
-
+      padding-top: 55px;
       .bg-card {
-        width: 80%;
+        width:60%;
       }
     }
-
     #search-container {
-      width: 15%;
-      // position: sticky;
-      // top: 0;
-
       .q-field__control {
         border-radius: 16px;
       }
-
       #filter-area {
         border: 1px solid #fff;
         border-radius: 16px;
