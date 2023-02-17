b46c31b5aad72c0a985716f2d15f7ac60a0bc1d7
Max <asd88922@gmail.com>
2023-02-17 16:32:12 +0800

    後端 user 路由新增 editMyself

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import content from '../middleware/content.js'
 import * as auth from '../middleware/auth.js'
-import { register, login, logout, extend, getMyself } from '../controllers/users.js'
+import { register, login, logout, extend, getMyself, editMyself } from '../controllers/users.js'
 
 const router = Router()
 
@@ -10,5 +10,6 @@ router.post('/login', content('application/json'), auth.login, login) // 登入
 router.delete('/logout', auth.jwt, logout) // 登出
 router.patch('/extend', auth.jwt, extend) // 過期 JWT 換新 JWT
 router.get('/myself', auth.jwt, getMyself) // 取自己的資料
+router.patch('/editmyself', auth.jwt, editMyself) // 改自己的資料
 
 export default router
