106a0577918898a03eb7003161030686dc2de9cd
Max <asd88922@gmail.com>
2023-02-19 00:47:24 +0800

    新增 取所有揪團、參加揪團(未完成)

--- back/controllers/teamups.js
+++ back/controllers/teamups.js
@@ -30,3 +30,57 @@ export const createTeamup = async (req, res) => {
     }
   }
 }
+
+export const getTeamup = async (req, res) => {
+  try {
+    const result = await teamups.findById(req.params.id)
+    if (!result) {
+      res.status(404).json({ success: false, message: '找不到此揪團' })
+    } else {
+      res.status(200).json({ success: true, message: '', result })
+    }
+  } catch (error) {
+    if (error.name === 'CastError') {
+      res.status(400).json({ success: false, message: 'ID 格式錯誤' })
+    } else {
+      res.status(500).json({ success: false, message: '未知錯誤' })
+    }
+  }
+}
+
+export const getAllTeamups = async (req, res) => {
+  try {
+    const result = await teamups.find()
+    res.status(200).json({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).json({ success: false, message: '未知錯誤' })
+  }
+}
+
+export const joinTeamup = async (req, res) => {
+  try {
+    const teamup = await teamups.findByIdAndUpdate(req.params.id)
+    if (!teamup) {
+      res.status(404).json({ success: false, message: '找不到此揪團' })
+    }
+    if (teamup.currentPeople >= teamup.totalPeople) {
+      res.status(400).json({ success: false, message: '揪團人數已滿' })
+    }
+    teamup.participant.push(req.user._id)
+    teamup.currentPeople++
+    await teamup.save()
+    res.status(200).json({ success: true, message: '參加揪團成功', teamup })
+  } catch (error) {
+    console.log(error)
+    if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400).json({ success: false, message })
+    } else if (error.name === 'MongoServerError' && error.code === 11000) {
+      // 代表重複
+      res.status(400).json({ success: false, message: '名稱重複' })
+    } else {
+      res.status(500).json({ success: false, message: '未知錯誤' })
+    }
+  }
+}
