846d13e996513423ab0e630cebd41fd1402fcfb4
Max <asd88922@gmail.com>
2023-02-19 00:47:35 +0800

    新增 取所有揪團、參加揪團(未完成)

--- back/routes/teamups.js
+++ back/routes/teamups.js
@@ -3,10 +3,13 @@ import content from '../middleware/content.js'
 import { jwt } from '../middleware/auth.js'
 import * as upload from '../middleware/upload.js'
 // import admin from '../middleware/admin.js'
-import { createTeamup } from '../controllers/teamups.js'
+import { createTeamup, getAllTeamups, getTeamup, joinTeamup } from '../controllers/teamups.js'
 
 const router = Router()
 
 router.post('/', content('multipart/form-data'), jwt, upload.boardgameImg, createTeamup)
+router.post('/:id', content('application/json'), jwt, joinTeamup)
+router.get('/all', jwt, getAllTeamups)
+router.get('/:id', getTeamup)
 
 export default router
