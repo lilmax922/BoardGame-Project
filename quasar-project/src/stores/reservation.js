01e08e69a08c9f8434646935a212cc12f363f82a
Max <asd88922@gmail.com>
2023-02-20 01:34:41 +0800

    新增 編輯API

--- quasar-project/src/stores/reservation.js
+++ quasar-project/src/stores/reservation.js
@@ -5,7 +5,8 @@ import { apiAuth } from 'src/boot/axios'
 
 export const useReservationStore = defineStore('reservation', () => {
   const reservations = reactive([])
-  async function submitReservation (form) {
+
+  async function submitReservation (form, _id) {
     try {
       if (form._id === '') {
         const { data } = await apiAuth.post('/reservations', form)
@@ -15,8 +16,49 @@ export const useReservationStore = defineStore('reservation', () => {
           title: '成功',
           text: '預約成功~'
         })
+      } else {
+        const { data } = await apiAuth.patch('/reservations/' + _id, form)
+        const index = reservations.findIndex(reservation => reservation._id === _id)
+        reservations[index] = data.result
+        Swal.fire({
+          icon: 'success',
+          title: '成功',
+          text: '修改成功'
+        })
       }
-      this.router.push('/')
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: '失敗',
+        text: error?.response?.data?.message || '發生錯誤'
+      })
+    }
+  }
+
+  const getAllReservations = async () => {
+    try {
+      const { data } = await apiAuth.get('/reservations/all')
+      reservations.splice(0, data.result.length, ...data.result)
+    } catch (error) {
+      console.log(error)
+      Swal.fire({
+        icon: 'error',
+        title: '失敗',
+        text: error?.response?.data?.message || '發生錯誤'
+      })
+    }
+  }
+
+  const deleteReservation = async (_id) => {
+    try {
+      await apiAuth.patch('/reservations/delete/' + _id)
+      const index = reservations.findIndex(reservation => reservation._id === _id)
+      reservations.splice(index, 1)
+      Swal.fire({
+        icon: 'success',
+        title: '成功',
+        text: '預約刪除成功'
+      })
     } catch (error) {
       Swal.fire({
         icon: 'error',
@@ -28,6 +70,8 @@ export const useReservationStore = defineStore('reservation', () => {
 
   return {
     reservations,
-    submitReservation
+    submitReservation,
+    getAllReservations,
+    deleteReservation
   }
 })
