f71f710b8adb6166e6b695e49d1bf3ea26115250
Max <asd88922@gmail.com>
2023-02-20 01:26:44 +0800

    新增 後端預約路由

--- back/routes/reservations.js
+++ back/routes/reservations.js
@@ -1,16 +1,16 @@
 import { Router } from 'express'
 import content from '../middleware/content.js'
 import { jwt } from '../middleware/auth.js'
-// import admin from '../middleware/admin.js'
-import { createReservation, getReservation } from '../controllers/reservations.js'
+import admin from '../middleware/admin.js'
+import { createReservation, getReservation, getAllReservations, editReservation, deleteReservation } from '../controllers/reservations.js'
 
 const router = Router()
 
 router.post('/getdate', content('application/json'), jwt, getReservation)
 router.post('/', content('application/json'), jwt, createReservation)
-// router.get('/all', jwt, getAllReservations)
-// router.get('/adminAll', jwt, admin, getAdminAllReservations)
+router.get('/all', jwt, admin, getAllReservations)
+router.patch('/delete/:id', content('application/json'), jwt, admin, deleteReservation)
+router.patch('/:id', content('application/json'), jwt, admin, editReservation)
 // router.get('/:id', jwt, getReservation)
-// router.patch('/delete/:id', content('application/json'), jwt, deleteReservation)
 
 export default router
