4ff87edeb6df314caf752f3f554422c66f2d9479
Max <asd88922@gmail.com>
2023-02-20 01:27:36 +0800

    編輯 AdminLayout 排版

--- quasar-project/src/layouts/AdminLayout.vue
+++ quasar-project/src/layouts/AdminLayout.vue
@@ -10,12 +10,12 @@ const { logout } = user
 const drawer = ref(false)
 
 const lists = [
-  { icon: 'mdi-account-edit', text: '帳號管理', to: '/admin/manageAccount' },
-  { icon: 'mdi-calendar-account', text: '預約管理', to: '/admin/manageReservation' },
-  { icon: 'mdi-account-group', text: '揪團管理', to: '/admin/manageTeamup' },
-  { icon: 'mdi-bullhorn', text: '活動管理', to: '/admin/manageEvent' },
   { icon: 'mdi-google-downasaur', text: '桌遊管理', to: '/admin/manageBG' },
-  { icon: 'mdi-home', text: '回首頁', to: '/' }
+  { icon: 'mdi-account-edit', text: '帳號管理', to: '/admin/manageAccount' },
+  { icon: 'mdi-calendar-edit', text: '預約管理', to: '/admin/manageReservation' },
+  { icon: 'mdi-account-group', text: '揪團管理', to: '/admin/manageTeamup' }
+  // { icon: 'mdi-bullhorn', text: '活動管理', to: '/admin/manageEvent' },
+  // { icon: 'mdi-home', text: '回首頁', to: '/' }
 ]
 </script>
 
@@ -39,27 +39,27 @@ q-layout(view='hHh Lpr lff')
             q-icon(:name="list.icon")
           q-item-section {{ list.text }}
         q-separator
-        q-item-label.q-pa-sm(overline) 前台編輯
+        //- q-item-label.q-pa-sm(overline) 前台編輯
         q-item(clickable v-ripple to="/")
           q-item-section(avatar)
-            q-icon(name='mdi-home-edit')
-          q-item-section 首頁
-        q-item(clickable v-ripple to="/editBoardgames")
-          q-item-section(avatar)
-            q-icon(name='mdi-puzzle-edit')
-          q-item-section 探索桌遊
-        q-item(clickable v-ripple)
-          q-item-section(avatar)
-            q-icon(name='mdi-dance-ballroom')
-          q-item-section 本期EVENT
-        q-item(clickable v-ripple)
-          q-item-section(avatar)
-            q-icon(name='mdi-account-group')
-          q-item-section 揪團組隊
-        q-item(clickable v-ripple)
-          q-item-section(avatar)
-            q-icon(name='mdi-calendar-edit')
-          q-item-section 手刀預約
+            q-icon(name='mdi-home')
+          q-item-section 回首頁
+    //-     q-item(clickable v-ripple to="/editBoardgames")
+    //-       q-item-section(avatar)
+    //-         q-icon(name='mdi-puzzle-edit')
+    //-       q-item-section 探索桌遊
+    //-     q-item(clickable v-ripple)
+    //-       q-item-section(avatar)
+    //-         q-icon(name='mdi-dance-ballroom')
+    //-       q-item-section 本期EVENT
+    //-     q-item(clickable v-ripple)
+    //-       q-item-section(avatar)
+    //-         q-icon(name='mdi-account-group')
+    //-       q-item-section 揪團組隊
+    //-     q-item(clickable v-ripple)
+    //-       q-item-section(avatar)
+    //-         q-icon(name='mdi-calendar-edit')
+    //-       q-item-section 手刀預約
     q-img.absolute-top(src='https://cdn.quasar.dev/img/material.png' style='height: 150px')
       .absolute-bottom.bg-transparent
         q-avatar.q-mb-sm(size='56px')
