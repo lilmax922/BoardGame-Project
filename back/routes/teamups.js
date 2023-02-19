5fe5fbc5690bf9e08881b526a3e041dc39df26ce
Max <asd88922@gmail.com>
2023-02-20 02:36:53 +0800

    新增揪團 修改/刪除 路由

--- back/routes/teamups.js
+++ back/routes/teamups.js
@@ -2,8 +2,8 @@ import { Router } from 'express'
 import content from '../middleware/content.js'
 import { jwt } from '../middleware/auth.js'
 import * as upload from '../middleware/upload.js'
-// import admin from '../middleware/admin.js'
-import { createTeamup, getAllTeamups, getTeamup, joinTeamup } from '../controllers/teamups.js'
+import admin from '../middleware/admin.js'
+import { createTeamup, getAllTeamups, getTeamup, joinTeamup, editTeamup, deleteTeamup } from '../controllers/teamups.js'
 
 const router = Router()
 
@@ -11,7 +11,7 @@ router.post('/', content('multipart/form-data'), jwt, upload.boardgameImg, creat
 router.post('/:id', content('application/json'), jwt, joinTeamup)
 router.get('/all', jwt, getAllTeamups)
 router.get('/:id', getTeamup)
-// router.patch('/delete/:id', content('application/json'), jwt, admin, deleteTeamup)
-// router.patch('/:id', content('application/json'), jwt, admin, editTeamup)
+router.patch('/delete/:id', content('application/json'), jwt, admin, deleteTeamup)
+router.patch('/:id', content('multipart/form-data'), jwt, admin, editTeamup)
 
 export default router
