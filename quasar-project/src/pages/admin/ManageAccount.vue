c7de85825c967f5daaaefef595ddd708a3f484b2
Max <asd88922@gmail.com>
2023-02-20 01:30:59 +0800

    新增 會員管理頁面

--- quasar-project/src/pages/admin/ManageAccount.vue
+++ quasar-project/src/pages/admin/ManageAccount.vue
@@ -1,11 +1,25 @@
 <script setup>
+import { storeToRefs } from 'pinia'
+import { useUserStore } from 'src/stores/user'
 
+// const user = useUserStore()
+// const users = storeToRefs(user)
 </script>
 
 <template>
-123
+  <q-page id="manage_account" padding>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h4 class="">帳號管理</h4>
+        </div>
+        <!-- account table -->
+        <div class="col-12">
+          <q-table title="Account Management"> </q-table>
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
