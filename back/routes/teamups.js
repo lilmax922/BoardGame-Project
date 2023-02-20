dc6391112ef15f4f4a8f3ecc60008d4c1872fcef
Max <asd88922@gmail.com>
2023-02-20 11:29:30 +0800

    修改後端揪團路由，加入參加與取消參加

--- back/routes/teamups.js
+++ back/routes/teamups.js
@@ -3,12 +3,12 @@ import content from '../middleware/content.js'
 import { jwt } from '../middleware/auth.js'
 import * as upload from '../middleware/upload.js'
 import admin from '../middleware/admin.js'
-import { createTeamup, getAllTeamups, getTeamup, joinTeamup, editTeamup, deleteTeamup } from '../controllers/teamups.js'
+import { createTeamup, getAllTeamups, getTeamup, joinOrCancelTeamup, editTeamup, deleteTeamup } from '../controllers/teamups.js'
 
 const router = Router()
 
 router.post('/', content('multipart/form-data'), jwt, upload.boardgameImg, createTeamup)
-router.post('/:id', content('application/json'), jwt, joinTeamup)
+router.post('/:id', jwt, joinOrCancelTeamup)
 router.get('/all', jwt, getAllTeamups)
 router.get('/:id', getTeamup)
 router.patch('/delete/:id', content('application/json'), jwt, admin, deleteTeamup)
