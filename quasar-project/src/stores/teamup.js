7526bb260ec0c28e384a99e1f2b84b2a609f0cd5
Max <asd88922@gmail.com>
2023-02-21 01:47:05 +0800

    use quasar Notify

--- quasar-project/src/stores/teamup.js
+++ quasar-project/src/stores/teamup.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
-import Swal from 'sweetalert2'
+import { Notify } from 'quasar'
 import { apiAuth } from 'src/boot/axios'
 
 export const useTeamupStore = defineStore('teamup', () => {
@@ -12,27 +12,30 @@ export const useTeamupStore = defineStore('teamup', () => {
       if (_id === '') {
         const { data } = await apiAuth.post('/teamups', form)
         teamups.push(data.result)
-        await Swal.fire({
-          icon: 'success',
-          title: '成功',
-          text: '揪團成功~'
+        Notify.create({
+          message: '揪團成功',
+          textColor: 'primary',
+          icon: 'mdi-emoticon-happy-outline',
+          color: 'white'
         })
       } else {
         const { data } = await apiAuth.patch('/teamups/' + _id, form)
-        const index = teamups.findIndex(teamup => teamup._id === _id)
+        const index = teamups.findIndex((teamup) => teamup._id === _id)
         teamups[index] = data.result
-        await Swal.fire({
-          icon: 'success',
-          title: '成功',
-          text: '修改成功'
+        Notify.create({
+          message: '修改成功',
+          textColor: 'primary',
+          icon: 'mdi-emoticon-happy-outline',
+          color: 'white'
         })
       }
-      // this.router.push('/')
     } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: '失敗',
-        text: error?.response?.data?.message || '發生錯誤'
+      Notify.create({
+        message: '資料取得失敗',
+        textColor: 'secondary',
+        color: 'white',
+        icon: 'mdi-emoticon-dead-outline',
+        caption: error?.response?.data?.message || '發生錯誤'
       })
     }
   }
@@ -43,46 +46,12 @@ export const useTeamupStore = defineStore('teamup', () => {
       const { data } = await apiAuth.get('/teamups/all')
       teamups.splice(0, teamups.length, ...data.result)
     } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: '失敗',
-        text: error?.response?.data?.message || '發生錯誤'
-      })
-    }
-  }
-
-  // 參加揪團
-  const joinTeamup = async (form) => {
-    try {
-      const { data } = await apiAuth.post('/teamups/' + form._id)
-      await Swal.fire({
-        icon: 'success',
-        title: '成功',
-        text: '參加成功~'
-      })
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: '失敗',
-        text: error?.response?.data?.message || '發生錯誤'
-      })
-    }
-  }
-
-  // 取消參加
-  const cancelTeamup = async (form) => {
-    try {
-      const { data } = await apiAuth.post('/teamups/' + form._id)
-      await Swal.fire({
-        icon: 'success',
-        title: '成功',
-        text: '取消成功~'
-      })
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: '失敗',
-        text: error?.response?.data?.message || '發生錯誤'
+      Notify.create({
+        message: '資料取得失敗',
+        textColor: 'secondary',
+        color: 'white',
+        icon: 'mdi-emoticon-dead-outline',
+        caption: error?.response?.data?.message || '發生錯誤'
       })
     }
   }
@@ -90,18 +59,21 @@ export const useTeamupStore = defineStore('teamup', () => {
   const deleteTeamup = async (_id) => {
     try {
       await apiAuth.patch('/teamups/delete/' + _id)
-      const index = teamups.findIndex(teamup => teamup._id === _id)
+      const index = teamups.findIndex((teamup) => teamup._id === _id)
       teamups.splice(index, 1)
-      Swal.fire({
-        icon: 'success',
-        title: '成功',
-        text: '預約刪除成功'
+      Notify.create({
+        message: '揪團刪除成功',
+        textColor: 'primary',
+        icon: 'mdi-emoticon-happy-outline',
+        color: 'white'
       })
     } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: '失敗',
-        text: error?.response?.data?.message || '發生錯誤'
+      Notify.create({
+        message: '資料取得失敗',
+        textColor: 'secondary',
+        color: 'white',
+        icon: 'mdi-emoticon-dead-outline',
+        caption: error?.response?.data?.message || '發生錯誤'
       })
     }
   }
@@ -109,8 +81,6 @@ export const useTeamupStore = defineStore('teamup', () => {
   return {
     teamups,
     submitTeamup,
-    joinTeamup,
-    cancelTeamup,
     getAllTeamups,
     deleteTeamup
   }
