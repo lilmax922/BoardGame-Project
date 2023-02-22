1e95e3b30d1203b003b1075e84dffd7163da6303
Max <asd88922@gmail.com>
2023-02-22 16:31:45 +0800

    新增 會員刪除參加揪團function

--- quasar-project/src/stores/teamup.js
+++ quasar-project/src/stores/teamup.js
@@ -75,11 +75,37 @@ export const useTeamupStore = defineStore('teamup', () => {
     }
   }
 
+  // 會員刪除發起的揪團
   const deleteMyTeamup = async (_id) => {
     try {
       console.log(_id)
       await apiAuth.patch('/teamups/delete/member/' + _id)
       const index = teamups.findIndex((teamup) => teamup._id === _id)
+      console.log(index)
+      teamups.splice(index, 1)
+      Notify.create({
+        message: '刪除成功',
+        textColor: 'primary',
+        icon: 'mdi-emoticon-happy-outline',
+        color: 'white'
+      })
+    } catch (error) {
+      Notify.create({
+        message: '資料刪除失敗',
+        textColor: 'secondary',
+        color: 'white',
+        icon: 'mdi-emoticon-dead-outline',
+        caption: error?.response?.data?.message || '發生錯誤'
+      })
+    }
+  }
+
+  // 會員刪除參加的揪團
+  const deleteMyJoinedTeamup = async (_id) => {
+    try {
+      console.log(_id)
+      await apiAuth.patch('/teamups/delete/member/' + _id)
+      const index = joinedTeamups.findIndex((teamup) => teamup._id === _id)
       console.log(index)
       teamups.splice(index, 1)
       Notify.create({
@@ -129,6 +155,7 @@ export const useTeamupStore = defineStore('teamup', () => {
     getAllTeamups,
     getMyTeamup,
     deleteTeamup,
-    deleteMyTeamup
+    deleteMyTeamup,
+    deleteMyJoinedTeamup
   }
 })
