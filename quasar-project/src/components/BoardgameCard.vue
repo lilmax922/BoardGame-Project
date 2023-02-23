e252ff37f56e25d0dcb940a241713ff212f65220
Max <asd88922@gmail.com>
2023-02-23 10:58:53 +0800

    delete

--- quasar-project/src/components/BoardgameCard.vue
+++ quasar-project/src/components/BoardgameCard.vue
@@ -33,36 +33,22 @@ defineProps({
 })
 </script>
 
-<!--
-q-card.bg-card(flat)
-  q-img(:src="cardImage" ratio=16/9)
-  q-card-section
-    span.type-tag(v-for="(type,idx) in types" :key="idx") &#35;{{ type }}&nbsp;
-    .text-h4.bg-name {{ name }}
-  q-separator(inset)
-  q-card-section.flex-center(horizontal)
-    q-card-section.col-8.q-pt-none
-      q-list
-        q-item.q-pa-none
-          q-item-section
-            q-icon(name="mdi-account-multiple" size="xs")
-          q-item-section(style="fontSize:18px") &nbsp;{{ players }} 人
-        q-item.q-pa-none
-          q-item-section.flex-auto
-            q-icon(name="mdi-clock" size="xs")
-          q-item-section &nbsp;約 {{ gameTime }} 分鐘
-    q-card-section.col-4.q-pa-none.flex.justify-center
-      q-btn.moreBtn(color="primary" :to="'/BGInfo/' + _id") 查看更多
- -->
 <template>
   <div id="bg_card">
     <q-card flat>
       <div class="image_area">
-        <q-img :ratio="4/3" :src="cardImage" />
+        <q-img :ratio="4 / 3" :src="cardImage" />
       </div>
       <q-card-section>
         <div class="types_area q-mb-sm">
-          <q-chip class="type_tag" v-for="(type, i) in types" :key="i" color="secondary" text-color="white" size="sm">
+          <q-chip
+            class="type_tag"
+            v-for="(type, i) in types"
+            :key="i"
+            color="secondary"
+            text-color="white"
+            size="sm"
+          >
             &#35;{{ type }}
           </q-chip>
         </div>
