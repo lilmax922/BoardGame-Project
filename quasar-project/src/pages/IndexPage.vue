560ac777b23900aefab2050f4fd22695e31e4215
Max <asd88922@gmail.com>
2023-02-23 14:48:41 +0800

    新增 Swiper

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -6,28 +6,28 @@ import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import { Navigation, Pagination } from 'swiper'
 import BoardgameCard from 'src/components/BoardgameCard.vue'
 import TeamupCard from 'src/components/TeamupCard.vue'
 import TestComp from 'src/components/TestComp.vue'
 import FaqAccordion from 'src/components/FaqAccordion.vue'
+import { useBoardgameStore } from 'src/stores/boardgame'
+import { useTeamupStore } from 'src/stores/teamup'
+import { storeToRefs } from 'pinia'
 
 const $q = useQuasar()
+const boardgameStore = useBoardgameStore()
+const teamupStore = useTeamupStore()
+const { getPostBoardgames } = boardgameStore
+const { getAllTeamups } = teamupStore
+const { boardgames } = storeToRefs(boardgameStore)
+const { teamups } = storeToRefs(teamupStore)
 
-const boardgames = reactive([])
-const slide = ref(1)
+getPostBoardgames()
+getAllTeamups()
 
-// ;(async () => {
-//   try {
-//     const { data } = api.get('/boardgames')
-//     boardgames.push(...data.result)
-//   } catch (error) {
-//     $q.notify({
-//       message: '失敗',
-//       caption: error?.response?.data?.message || '發生錯誤',
-//       color: 'secondary'
-//     })
-//   }
-// })()
+// Swiper
+const modules = [Navigation, Pagination]
 </script>
 
 <template>
@@ -36,7 +36,7 @@ const slide = ref(1)
       <div class="glass row">
         <div class="title col-12 text-h3">關於我們</div>
         <div class="content col-12 q-pt-md text-h6">
-          在這個科技冷漠的世代，我們希望成為一個讓人們聚在一起，享受桌遊樂趣的地方，並透過桌遊這個媒介，讓人們能夠在日常生活中找到快樂和樂趣。
+          <!-- 在這個科技冷漠的世代，我們希望成為一個讓人們聚在一起，享受桌遊樂趣的地方，並透過桌遊這個媒介，讓人們能夠在日常生活中找到快樂和樂趣。 -->
         </div>
       </div>
     </div>
@@ -46,23 +46,84 @@ const slide = ref(1)
         <div class="row">
           <div class="col-12">
             <div class="title text-h4">大家都在玩</div>
-            123456
-            <swiper
-              class="height"
-              data-aos="fade-up"
-              :modules="modules"
-              :spaceBetween="40"
-              :slidesPerView="'auto'"
-            >
-              <swiper-slide v-for="i in boardgames" :key="i">
-                <BoardgameCard v-bind="i" />
-              </swiper-slide>
-            </swiper>
+            <!-- boardgame Swiper Area -->
+            <div class="q-mt-lg">
+              <Swiper
+                class="mySwiper"
+                :modules="modules"
+                :navigation="true"
+                :pagination="true"
+                :slidesPerView="1"
+                :spaceBetween="30"
+                :breakpoints="{
+                  '576': {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  '768': {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                  },
+                  '1024': {
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                  },
+                  '1200': {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                  }
+                }"
+              >
+                <SwiperSlide v-for="i in boardgames" :key="i">
+                  <BoardgameCard v-bind="i" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </section>
 
       <section id="section02">
+        <div class="row">
+          <div class="col-12">
+            <div class="title text-h4">大家都在揪</div>
+            <div class="q-mt-lg">
+              <Swiper
+                class="mySwiper"
+                :modules="modules"
+                :navigation="true"
+                :pagination="true"
+                :slidesPerView="1"
+                :spaceBetween="30"
+                :breakpoints="{
+                  '576': {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  '768': {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                  },
+                  '1024': {
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                  },
+                  '1200': {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                  }
+                }"
+              >
+                <SwiperSlide v-for="i in teamups" :key="i">
+                  <TeamupCard v-bind="i" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="section03">
         <div class="row">
           <div class="col-12">
             <div class="title text-h4">常見問題</div>
@@ -78,6 +139,10 @@ const slide = ref(1)
 
 <style lang="scss">
 #index {
+  #section01 ~ section {
+    margin-top: 4rem;
+  }
+
   .hero {
     position: relative;
     height: 75vh;
