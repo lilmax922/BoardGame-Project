31ad98a76e3ae92336fa2565297e1139b024d6c9
Max <asd88922@gmail.com>
2023-02-26 01:49:57 +0800

    add AOS

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -2,10 +2,12 @@
 import { ref, reactive } from 'vue'
 import { useQuasar } from 'quasar'
 import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Navigation, Pagination } from 'swiper'
+import { Navigation, Pagination, Autoplay } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import AOS from 'aos'
+import '../../node_modules/aos/dist/aos.css'
 import { storeToRefs } from 'pinia'
 import { useBoardgameStore } from 'src/stores/boardgame'
 import { useTeamupStore } from 'src/stores/teamup'
@@ -13,6 +15,8 @@ import BoardgameCard from 'src/components/BoardgameCard.vue'
 import TeamupCard from 'src/components/TeamupCard.vue'
 import TestComp from 'src/components/TestComp.vue'
 import FaqAccordion from 'src/components/FaqAccordion.vue'
+
+AOS.init()
 
 const $q = useQuasar()
 const boardgameStore = useBoardgameStore()
@@ -31,45 +35,100 @@ getAllTeamups()
 // })
 
 // Swiper
-const modules = [Navigation, Pagination]
+const modules = [Navigation, Pagination, Autoplay]
 </script>
 
 <template>
   <q-page id="index">
-    <div class="hero">
-      <!-- <div class="glass row">
-            <div class="title col-12 text-h3">關於我們</div>
-            <div class="content col-12 q-pt-md text-h6">
-              在這個科技冷漠的世代，我們希望成為一個讓人們聚在一起，享受桌遊樂趣的地方，並透過桌遊這個媒介，讓人們能夠在日常生活中找到快樂和樂趣。
+    <div class="hero flex flex-center">
+      <div class="container">
+        <div class="text-center">
+          <p class="hero-title text-h2">
+            Board games bring people closer together.
+          </p>
+          <div class="flex flex-center q-gutter-md q-mt-lg">
+            <div>
+              <q-btn
+                class="hero_btn"
+                to="/reservation"
+                label="我要預約"
+              ></q-btn>
             </div>
-          </div> -->
+            <div>
+              <q-btn class="hero_btn" to="/teamup" label="我要揪團"></q-btn>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <section id="about_section">
       <div class="container">
         <div class="row">
-          <div class="about_title text-h5">關於揪遊Lego</div>
-          <p class="about_content col-12 q-pt-md text-h6 text-center">
-            在這個科技冷漠的世代，我們希望成為一個讓人們聚在一起，享受桌遊樂趣的地方，並透過桌遊這個媒介，讓人們能夠在日常生活中找到快樂和樂趣。
+          <div class="col-12 text-center">
+            <p
+              class="about text-h6"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+            >
+              關於揪遊Lego
+            </p>
+          </div>
+          <p
+            class="about_title col-12 text-h5 text-accent text-center text-weight-bold"
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            data-aos-easing="ease-in-sine"
+          >
+            桌上遊戲，讓我們拉近人與人的距離。
+          </p>
+          <p
+            class="about_content col-12 q-pt-md text-h6 text-weight-medium text-center"
+            data-aos="fade-left"
+            data-aos-duration="1500"
+            data-aos-easing="ease-in-sine"
+          >
+            一個能夠聚在一起享受桌遊樂趣的地方，透過桌遊，讓人們能夠在揪遊Lego獲得快樂和樂趣。
           </p>
         </div>
       </div>
     </section>
 
-    <!-- <div class="container"> -->
+    <!-- 店內桌遊 parallax -->
+    <section>
+      <div class="row">
+        <div class="col-12">
+          <q-parallax src="../assets/店內桌遊.jpg"> </q-parallax>
+        </div>
+      </div>
+    </section>
 
     <!-- 桌遊 -->
-    <section id="section01">
+    <section id="boardgame_section">
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <div class="title text-h4">
+            <div
+              data-aos="fade-right"
+              data-aos-duration="1500"
+              class="title text-h4"
+            >
               大家都在<span class="text-accent">玩</span>
             </div>
             <!-- boardgame Swiper Area -->
             <div class="content">
-              <Swiper class="mySwiper" :modules="modules" :navigation="true" :pagination="true" :slidesPerView="1"
-                :spaceBetween="30" :breakpoints="{
+              <Swiper
+                class="mySwiper"
+                :modules="modules"
+                :navigation="true"
+                :autoplay="{
+                  delay: 3000,
+                  disableOnInteraction: false,
+                }"
+                :loop="true"
+                :slidesPerView="1"
+                :spaceBetween="30"
+                :breakpoints="{
                   '576': {
                     slidesPerView: 1,
                     spaceBetween: 20,
@@ -86,7 +145,8 @@ const modules = [Navigation, Pagination]
                     slidesPerView: 4,
                     spaceBetween: 50,
                   },
-                }">
+                }"
+              >
                 <SwiperSlide v-for="i in boardgames" :key="i">
                   <BoardgameCard v-bind="i" />
                 </SwiperSlide>
@@ -98,33 +158,48 @@ const modules = [Navigation, Pagination]
     </section>
 
     <!-- 揪團 -->
-    <section id="section02">
+    <section id="teamup_section">
       <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <div class="title text-h4">
-            大家都在<span class="text-accent">揪</span>
-          </div>
-          <div class="content">
-            <Swiper class="mySwiper" :modules="modules" :navigation="true" :pagination="true" :slidesPerView="1"
-              :spaceBetween="30" :breakpoints="{
-                '576': {
-                  slidesPerView: 1,
-                  spaceBetween: 20,
-                },
-                '768': {
-                  slidesPerView: 2,
-                  spaceBetween: 30,
-                },
-                '1024': {
-                  slidesPerView: 3,
-                  spaceBetween: 40,
-                },
-                '1200': {
-                  slidesPerView: 4,
-                  spaceBetween: 50,
-                },
-              }">
+        <div class="row">
+          <div class="col-12">
+            <div
+              class="title text-h4"
+              data-aos="fade-right"
+              data-aos-duration="1500"
+            >
+              大家都在<span class="text-accent">揪</span>
+            </div>
+            <div class="content">
+              <Swiper
+                class="mySwiper"
+                :modules="modules"
+                :navigation="true"
+                :autoplay="{
+                  delay: 3000,
+                  disableOnInteraction: false,
+                }"
+                :loop="true"
+                :slidesPerView="1"
+                :spaceBetween="30"
+                :breakpoints="{
+                  '576': {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  '768': {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                  },
+                  '1024': {
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                  },
+                  '1200': {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                  },
+                }"
+              >
                 <SwiperSlide v-for="i in teamups" :key="i">
                   <TeamupCard v-bind="i" />
                 </SwiperSlide>
@@ -136,21 +211,27 @@ const modules = [Navigation, Pagination]
     </section>
 
     <!-- <section>
-        <div class="container">
-          <div class="row">
-            <div class="col-12">
-              <div>本期EVENT</div>
-            </div>
-          </div>
-        </div>
-      </section> -->
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-12">
+                        <div>本期EVENT</div>
+                      </div>
+                    </div>
+                  </div>
+                </section> -->
 
     <!-- FAQ -->
-    <section>
+    <section id="faq_section">
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <div class="title text-h4">常見問題</div>
+            <div
+              class="text-center"
+              data-aos="fade-up"
+              data-aos-duration="3000"
+            >
+              <div class="faq text-h6">常見問題</div>
+            </div>
             <div class="content">
               <FaqAccordion />
             </div>
@@ -158,7 +239,12 @@ const modules = [Navigation, Pagination]
         </div>
       </div>
     </section>
-    <!-- </div> -->
+
+      <section>
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/25OpgUg5qCI" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </section>
   </q-page>
 </template>
 
@@ -167,10 +253,31 @@ const modules = [Navigation, Pagination]
   .hero {
     position: relative;
     width: 100%;
-    height: 100vh;
-    background: url("../assets/images/hero/mix1.png") no-repeat;
+    height: 75vh;
+    background: url("../assets/images/hero/boardgames-banner-image.png")
+      no-repeat;
     background-size: cover;
     background-position: 50%;
+
+    .hero-title {
+      font-weight: 700;
+    }
+
+    .hero_btn {
+      width: 200px;
+      color: #fff;
+      border: 1px solid #fff;
+
+      &:hover {
+        transition: 0.5s;
+        color: $dark;
+        background-color: #fff;
+      }
+    }
+
+    .hero_content {
+      color: $textColor;
+    }
 
     .glass {
       position: absolute;
@@ -179,9 +286,11 @@ const modules = [Navigation, Pagination]
       width: 35%;
       padding: 1rem;
       backdrop-filter: blur(10px);
-      background: linear-gradient(135deg,
-          rgba($color: #fff, $alpha: 0.1),
-          rgba($color: #fff, $alpha: 0));
+      background: linear-gradient(
+        135deg,
+        rgba($color: #fff, $alpha: 0.1),
+        rgba($color: #fff, $alpha: 0)
+      );
       border-radius: 16px;
       border: 1px solid rgba($color: #fff, $alpha: 0.18);
       box-shadow: 0 8px 32px 0 rgba($color: $dark, $alpha: 0.37);
@@ -202,42 +311,52 @@ const modules = [Navigation, Pagination]
     }
   }
 
-  #about_section {
-    margin: 150px 0;
-
-    .row {
-      width: 100%;
-      display: flex;
-      justify-content: center;
-
-      .about_title {
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-        font-weight: 900;
-        border-bottom: 1px solid #fff;
-        padding-bottom: 2px;
-      }
-
-      .about_content {
-        display: flex;
-        max-width: 750px;
-        margin-top: 100px;
-        color: $text-color;
-        font-weight: 700;
-      }
-    }
-  }
-
   .container {
     .title {
       border-left: 15px solid $primary;
       padding-left: 1rem;
+      // text-align: center;
+      // border-bottom: 1px solid #fff;
+      // padding-bottom: 2px;
+      // display: inline-block;
     }
 
     .content {
       margin: 2rem 0 4.5rem 0;
     }
+  }
+
+  .faq {
+    font-weight: 900;
+    border-bottom: 1px solid #fff;
+    padding-bottom: 2px;
+    display: inline-block;
+  }
+
+  #about_section {
+    margin: 150px 0;
+
+    .row {
+      width: 100%;
+      justify-content: center;
+      gap: 1.5rem;
+
+      .about {
+        display: inline-block;
+        font-weight: 900;
+        border-bottom: 1px solid #fff;
+        padding-bottom: 2px;
+      }
+
+      .about_title,
+      .about_content {
+        color: $textColor;
+      }
+    }
+  }
+
+  #boardgame_section {
+    margin-top: 50px;
   }
 }
 </style>
