43c712213e13db3efcc32a30950780eeb229e60b
Max <asd88922@gmail.com>
2023-02-28 06:13:22 +0800

    更改登入樣式

--- quasar-project/src/components/LoginCard.vue
+++ quasar-project/src/components/LoginCard.vue
@@ -40,27 +40,55 @@ q-card#login-card(flat style="width:1000px")
     q-card-section.col-8.text-center
       h5.text-center 會員登入
       q-form(@submit="login").q-gutter-sm
-        q-input(filled v-model='loginForm.email' label='電子信箱' :rules="[rules.required ,rules.email]")
+        q-input(rounded standout clearable v-model='loginForm.email' label='電子信箱' :rules="[rules.required ,rules.email]")
           template(v-slot:prepend)
             q-icon(name="mdi-email")
-        q-input(filled counter maxlength="12" v-model='loginForm.password' label='密碼' :rules="[rules.password, rules.required, rules.length]" :type="loginForm.isPwd ? 'password' : 'text'")
+        q-input(rounded standout counter maxlength="12" v-model='loginForm.password' label='密碼' :rules="[rules.password, rules.required, rules.length]" :type="loginForm.isPwd ? 'password' : 'text'")
           template(v-slot:prepend)
             q-icon(name="mdi-lock")
           template(v-slot:append)
             q-icon.cursor-pointer(:name="loginForm.isPwd ? 'visibility_off' : 'visibility'" @click="loginForm.isPwd = !loginForm.isPwd")
-        q-btn(label="登入" type="submit" color="primary")
+        q-btn.confirm_btn(label="登入" type="submit")
 
     q-separator
 
     q-card-section.col-4.flex-center.column
+      q-img.q-mb-md(src="../assets/logo.png")
       q-btn.close-btn(icon="close" flat v-close-popup)
       p 還沒建立帳號嗎?
-      q-btn(outline label="註冊" color="white" @click="emit('showRegisterCard', true)")
+      q-btn.action_btn(label="註冊" @click="emit('showRegisterCard', true)")
 </template>
 
 <style lang="scss" scoped>
   .close-btn {
-    top: -30%;
+    top: -50%;
     left: 35%;
   }
+
+  .action_btn {
+    padding: 5px 3px;
+      width: 100px;
+      border-radius: 8px;
+      border: 1px solid #fff;
+
+      &:hover {
+        transition: 0.5s;
+        color: $dark;
+        background-color: #fff;
+      }
+  }
+  .confirm_btn{
+      padding: 5px 3px;
+      width: 100px;
+      color: $dark;
+      background-color: $primary;
+      border-radius: 8px;
+
+      &:hover {
+        transition: 0.5s;
+        color: $primary;
+        background-color: $dark;
+        border: 1px solid $primary;
+      }
+    }
 </style>
