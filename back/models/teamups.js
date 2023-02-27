554978b8565741be2fff74a18f64e140369bf58d
Max <asd88922@gmail.com>
2023-02-28 05:47:45 +0800

    標題最多30個字，內容最少1個字

--- back/models/teamups.js
+++ back/models/teamups.js
@@ -40,21 +40,6 @@ const schema = new Schema(
       type: String,
       required: [true, '缺少主要遊玩類型圖片']
     },
-    // type: {
-    //   type: String,
-    //   required: [true, '缺少主要遊玩類型'],
-    //   enum: {
-    //     values: ['派對', '陣營', '策略', '心機', '卡牌', '兒童', '家庭', '抽象'],
-    //     message: '桌遊類型錯誤'
-    //   },
-    //   validate: {
-    //     validator () {
-    //       return _.intersection(this.type, this.interesting).length === 0
-    //     },
-    //     message: '錯誤'
-    //   },
-    //   default: []
-    // },
     types: {
       type: [String],
       enum: {
@@ -72,12 +57,13 @@ const schema = new Schema(
     title: {
       type: String,
       required: [true, '缺少揪團標題'],
-      maxLength: [20, '標題最多 20 個字'],
+      maxLength: [30, '標題最多 30 個字'],
       minLength: [1, '標題最少 1 個字']
     },
     content: {
       type: String,
-      required: [true, '缺少揪團內容']
+      required: [true, '缺少揪團內容'],
+      minLength: [1, '內容最少 1 個字']
     }
   }, { versionKey: false })
 
