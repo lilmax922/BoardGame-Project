abce5ba5e1e3a5526827585bdaf272dd1ffabde0
Max <asd88922@gmail.com>
2023-02-28 06:13:27 +0800

    更改註冊樣式

--- quasar-project/src/components/RegisterCard.vue
+++ quasar-project/src/components/RegisterCard.vue
@@ -66,44 +66,72 @@ const registerHandler = async () => {
 q-card#register-card(flat style="width:1500px")
   q-card-section(horizontal)
     q-card-section.col-4.flex-center.column
-      q-btn.close-btn(icon="close" flat v-close-popup)
-      p 已經註冊過了?
-      q-btn(outline label="登入" color="white" @click="emit('showRegisterCard', false)")
-        //- q-img(src="../assets/MAXXD.png" style="background:#EEE3D2; border-radius:16px")
+      .flex.justify-center
+        q-img.q-mb-xl(src="../assets/logo.png")
+        q-btn.close-btn(icon="close" flat v-close-popup)
+        p 已經註冊過了?
+        q-btn.action_btn(label="登入" @click="emit('showRegisterCard', false)")
 
     q-separator
 
     q-card-section.col-8.text-center
       h5.text-center 會員註冊
       q-form(@submit="registerHandler").q-gutter-sm
-        q-input.bottom-slots(filled counter maxlength="不限" v-model='registerForm.email' label='電子信箱' :rules="[rules.required ,rules.email,]")
+        q-input.bottom-slots(rounded standout clearable counter maxlength="不限" v-model='registerForm.email' label='電子信箱' :rules="[rules.required ,rules.email,]")
           template(v-slot:prepend)
             q-icon(name="mdi-email")
           template(v-slot:append)
             q-icon(v-if="text !== ''" name="fa-solid fa-xmark" @click="text = ''" class="cursor-pointer")
-        q-input(filled counter maxlength="10" v-model="registerForm.phone" label="手機號碼" :rules="[rules.required, rules.phone]")
+        q-input(rounded standout clearable counter maxlength="10" v-model="registerForm.phone" label="手機號碼" :rules="[rules.required, rules.phone]")
           template(v-slot:prepend)
             q-icon(name="mdi-cellphone")
-        q-input(filled counter maxlength="20" v-model="registerForm.nickname" label="暱稱" :rules="[rules.required, rules.nickname]")
+        q-input(rounded standout clearable counter maxlength="20" v-model="registerForm.nickname" label="暱稱" :rules="[rules.required, rules.nickname]")
           template(v-slot:prepend)
             q-icon(name="mdi-draw")
-        q-input(filled counter maxlength="12" v-model='registerForm.password' label='密碼' :rules="[rules.required, rules.password, rules.length]" :type="registerForm.isPwd ? 'password' : 'text'")
+        q-input(rounded standout counter maxlength="12" v-model='registerForm.password' label='密碼' :rules="[rules.required, rules.password, rules.length]" :type="registerForm.isPwd ? 'password' : 'text'")
           template(v-slot:prepend)
             q-icon(name="mdi-lock")
           template(v-slot:append)
             q-icon.cursor-pointer(:name="registerForm.isPwd ? 'visibility_off' : 'visibility'" @click="registerForm.isPwd = !registerForm.isPwd")
-        q-input(filled counter maxlength="12" v-model='registerForm.confirmPassword' label='確認密碼' :rules="[rules.required, rules.confirmPassword, rules.length]" :type="registerForm.isConfirmPwd ? 'password' : 'text'")
+        q-input(rounded standout counter maxlength="12" v-model='registerForm.confirmPassword' label='確認密碼' :rules="[rules.required, rules.confirmPassword, rules.length]" :type="registerForm.isConfirmPwd ? 'password' : 'text'")
           template(v-slot:prepend)
             q-icon(name="mdi-check-all")
           template(v-slot:append)
             q-icon.cursor-pointer(:name="registerForm.isConfirmPwd ? 'visibility_off' : 'visibility'" @click="registerForm.isConfirmPwd = !registerForm.isConfirmPwd")
-        q-btn(label="註冊" type="submit" color="primary")
+        q-btn.confirm_btn(label="註冊" type="submit")
 </template>
 
 <style lang="scss" scoped>
   .close-btn {
-    top: -38%;
+    top: -120%;
     left: 275%;
     z-index: 1;
   }
+
+  .action_btn {
+    padding: 5px 3px;
+      width: 100px;
+      border-radius: 8px;
+      border: 1px solid #fff;
+
+      &:hover {
+        transition: 0.5s;
+        color: $dark;
+        background-color: #fff;
+      }
+  }
+  .confirm_btn{
+      padding: 5px 3px;
+      width: 100px;
+      color: $dark;
+      background-color: $primary;
+      border-radius: 8px;
+
+      &:hover {
+        transition: 0.5s;
+        color: $primary;
+        background-color: $dark;
+        border: 1px solid $primary;
+      }
+    }
 </style>
