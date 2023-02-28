6d139fcf6391385b66ac0e38719b6af5fae3c7ec
Max <asd88922@gmail.com>
2023-02-28 16:55:58 +0800

    新增QPageScroller

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -12,7 +12,11 @@ const { logout } = user
 const showRegisterCard = ref(false)
 const memberDropdown = [
   { icon: 'mdi-account-edit', text: '會員資料', to: '/member/myInfo' },
-  { icon: 'mdi-google-downasaur', text: '我的預約', to: '/member/myReservation' },
+  {
+    icon: 'mdi-google-downasaur',
+    text: '我的預約',
+    to: '/member/myReservation'
+  },
   { icon: 'mdi-account-group', text: '我的揪團', to: '/member/myTeamup' }
 ]
 const adminDropdown = [
@@ -46,13 +50,12 @@ const close = () => {
 //     }
 //   })
 // })
-
 </script>
 
 <template>
   <q-layout id="mainlayout" view="hHh lpR fff">
-    <q-header  class="header flex flex-center" reveal>
-      <q-toolbar class="row" style="width: 100%;">
+    <q-header class="header flex flex-center" reveal>
+      <q-toolbar class="row" style="width: 100%">
         <q-toolbar-title class="col-xs-4 col-md-3">
           <router-link to="/">
             <img class="logo" src="../assets/logo.png" />
@@ -60,7 +63,11 @@ const close = () => {
         </q-toolbar-title>
 
         <!-- 分頁 -->
-        <q-tabs active-color="primary" dense class="gt-md col-md-6 flex items-end">
+        <q-tabs
+          active-color="primary"
+          dense
+          class="gt-md col-md-6 flex items-end"
+        >
           <q-route-tab to="/" label="首頁" />
           <q-route-tab to="/exploreBGs" label="探索桌遊" />
           <q-route-tab to="/searchTeamups" label="揪團組隊" />
@@ -86,7 +93,14 @@ const close = () => {
             </q-btn> -->
 
           <!-- 登入/註冊 btn -->
-          <q-btn v-if="!isLogin" @click="showLoginCard = true" icon="mdi-account-circle" flat dense size="lg" />
+          <q-btn
+            v-if="!isLogin"
+            @click="showLoginCard = true"
+            icon="mdi-account-circle"
+            flat
+            dense
+            size="lg"
+          />
           <!-- 使用者下拉選單 -->
           <q-btn-dropdown v-if="isLogin" class="q-ml-sm" flat>
             <template #label>
@@ -106,72 +120,106 @@ const close = () => {
                 </q-item-section>
               </q-item>
               <q-separator />
-              <q-item-label v-if="!isAdmin" class="q-pa-sm" overline>我的主頁</q-item-label>
-              <q-item-label v-if="isAdmin" class="q-pa-sm" overline>後台管理</q-item-label>
+              <q-item-label v-if="!isAdmin" class="q-pa-sm" overline
+                >我的主頁</q-item-label
+              >
+              <q-item-label v-if="isAdmin" class="q-pa-sm" overline
+                >後台管理</q-item-label
+              >
               <!-- 管理者 -->
               <div v-if="isLogin && isAdmin">
-              <q-item v-for="i in adminDropdown" :key="i" :to="i.to" clickable>
-                <q-item-section avatar>
-                  <q-icon :name="i.icon" />
-                </q-item-section>
-                <q-item-section>{{ i.text }}</q-item-section>
-              </q-item>
-            </div>
+                <q-item
+                  v-for="i in adminDropdown"
+                  :key="i"
+                  :to="i.to"
+                  clickable
+                >
+                  <q-item-section avatar>
+                    <q-icon :name="i.icon" />
+                  </q-item-section>
+                  <q-item-section>{{ i.text }}</q-item-section>
+                </q-item>
+              </div>
 
               <!-- 會員 -->
-              <div  v-if="!isAdmin">
-              <q-item v-for="i in memberDropdown" :key="i" :to="i.to" clickable>
-                <q-item-section avatar>
-                  <q-icon :name="i.icon" />
+              <div v-if="!isAdmin">
+                <q-item
+                  v-for="i in memberDropdown"
+                  :key="i"
+                  :to="i.to"
+                  clickable
+                >
+                  <q-item-section avatar>
+                    <q-icon :name="i.icon" />
+                  </q-item-section>
+                  <q-item-section>{{ i.text }}</q-item-section>
+                </q-item>
+              </div>
+
+              <q-separator v-if="isLogin" />
+
+              <q-item v-if="isLogin">
+                <q-item-section>
+                  <q-btn
+                    @click="logout"
+                    icon="fa-solid fa-person-walking-arrow-right"
+                    label="登出"
+                    flat
+                    dense
+                  />
                 </q-item-section>
-                <q-item-section>{{ i.text }}</q-item-section>
-              </q-item>
-            </div>
-
-            <q-separator v-if="isLogin" />
-
-            <q-item v-if="isLogin">
-              <q-item-section>
-                <q-btn @click="logout" icon="fa-solid fa-person-walking-arrow-right" label="登出" flat dense />
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
-
-        <q-btn class="lt-lg" dense flat round icon="menu">
-          <q-menu transition-show="jump-down" transition-hide="jump-up">
-            <q-list style="min-width: 100px">
-              <q-item clickable to="/">
-                <q-item-section>首頁</q-item-section>
-              </q-item>
-              <q-item clickable to="/exploreBGs">
-                <q-item-section>探索桌遊</q-item-section>
-              </q-item>
-              <q-item clickable to="/searchTeamups">
-                <q-item-section>揪團組隊</q-item-section>
-              </q-item>
-              <q-item clickable to="/teamup">
-                <q-item-section>我要揪團</q-item-section>
-              </q-item>
-              <q-item clickable to="/reservation">
-                <q-item-section>手刀預約</q-item-section>
               </q-item>
             </q-list>
-          </q-menu>
+          </q-btn-dropdown>
 
-        </q-btn>
-      </div>
-
-    </q-toolbar>
-  </q-header>
+          <q-btn class="lt-lg" dense flat round icon="menu">
+            <q-menu transition-show="jump-down" transition-hide="jump-up">
+              <q-list style="min-width: 100px">
+                <q-item clickable to="/">
+                  <q-item-section>首頁</q-item-section>
+                </q-item>
+                <q-item clickable to="/exploreBGs">
+                  <q-item-section>探索桌遊</q-item-section>
+                </q-item>
+                <q-item clickable to="/searchTeamups">
+                  <q-item-section>揪團組隊</q-item-section>
+                </q-item>
+                <q-item clickable to="/teamup">
+                  <q-item-section>我要揪團</q-item-section>
+                </q-item>
+                <q-item clickable to="/reservation">
+                  <q-item-section>手刀預約</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
 
     <q-page-container>
       <router-view />
+      <q-page-scroller
+        position="bottom-right"
+        :scroll-offset="2000"
+        :offset="[18, 18]"
+        duration="500"
+      >
+        <q-btn fab icon="mdi-arrow-up" color="primary" />
+      </q-page-scroller>
     </q-page-container>
 
     <q-dialog v-model="showLoginCard" persistent>
-      <LoginCard v-if="!showRegisterCard" @showRegisterCard="toggleRegisterCardHandler" @closeDialog="close" />
-      <RegisterCard v-if="showRegisterCard" @showRegisterCard="toggleRegisterCardHandler" @closeDialog="close" />
+      <LoginCard
+        v-if="!showRegisterCard"
+        @showRegisterCard="toggleRegisterCardHandler"
+        @closeDialog="close"
+      />
+      <RegisterCard
+        v-if="showRegisterCard"
+        @showRegisterCard="toggleRegisterCardHandler"
+        @closeDialog="close"
+      />
     </q-dialog>
 
     <q-footer class="bg-primary text-dark">
@@ -179,7 +227,9 @@ const close = () => {
         <q-avatar class="q-mr-sm">
           <img src="../assets/logo.png" />
         </q-avatar>
-        <span class="text-center" style="font-size: 14px">©2023 泰山職訓 網頁專題設計 by 胡俊宇</span>
+        <span class="text-center" style="font-size: 14px"
+          >©2023 泰山職訓 網頁專題設計 by 胡俊宇</span
+        >
       </q-toolbar>
     </q-footer>
   </q-layout>
@@ -189,39 +239,39 @@ const close = () => {
 #mainlayout {
   width: 100%;
   .header {
-  width: 100%;
-  background-color: rgba(255, 255, 255, 0);
-
-  .logo {
-    height: 90%;
-    position: absolute;
-    top: 10%;
-    left: 6.5%;
-    filter: drop-shadow(0px 0px 3px $primary);
-  }
-
-  .q-toolbar {
     width: 100%;
-    height: 80px;
-  }
+    background-color: rgba(255, 255, 255, 0);
 
-  .q-tabs {
-    color: #fff;
-  }
+    .logo {
+      height: 90%;
+      position: absolute;
+      top: 10%;
+      left: 6.5%;
+      filter: drop-shadow(0px 0px 3px $primary);
+    }
 
-  .q-tab__label {
-    font-size: 16px;
-    font-weight: 500;
+    .q-toolbar {
+      width: 100%;
+      height: 80px;
+    }
 
-    &:hover {
-      color: $primary;
-      transition: 0.5s;
+    .q-tabs {
+      color: #fff;
+    }
+
+    .q-tab__label {
+      font-size: 16px;
+      font-weight: 500;
+
+      &:hover {
+        color: $primary;
+        transition: 0.5s;
+      }
+    }
+
+    .q-tab__indicator {
+      height: 3.5px;
     }
   }
-
-  .q-tab__indicator {
-    height: 3.5px;
-  }
-}
 }
 </style>
